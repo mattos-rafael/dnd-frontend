d6cb2b4614b6660aec564ad7f3016c504c6e73aa
mattos-rafael <rafa.rafa.rafa07@gmail.com>
2026-05-11 13:27:39 +0200

    form done

--- src/components/CreateCharacterForm/CreateCharacterForm.jsx
+++ src/components/CreateCharacterForm/CreateCharacterForm.jsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import ButtonForm from "../ButtonForm/ButtonForm"
 import CheckboxForm from "../CheckboxForm/CheckboxForm"
+import { UNSAFE_createClientRoutesWithHMRRevalidationOptOut } from "react-router-dom"
 
 function CreateCharacterForm() {
   const [url, setUrl] = useState('https://www.dnd5eapi.co/api/races')
@@ -16,21 +17,37 @@ function CreateCharacterForm() {
   const [traits, setTraits] = useState([])
   const [lenguages, setLenguages] = useState([])
   const [answers, setAnswers]= useState([])
+  const [classesList, setClassesList] = useState([])
+  const [classes, setClasses] = useState('')
+  const [classInfo, setClassInfo] = useState({})
+  const [hitDie, setHitDie] = useState(0)
+  const [proficiencies, setProficiencies] = useState([])
+  const [equipment, setEquipment] = useState([])
+  const [atributes, setAtributes] = useState({})
 
 
   //state to save the bonus abilities
 
   //pass the function to save the abilities in the higher state
-const changeAbilityBonus=(e) =>{//{name,bonus}
+const changeAbilityBonus=(e, source) =>{//{name,bonus}
   //check action
   //e.target.checked if answers.length>= ability_bonus_options.choose return 
   //else // setAnswers([...answers,bonusObject])
   //uncheck action
-  if(answers.length < raceInfo?.ability_bonus_options?.choose) {
-    console.log(raceInfo?.ability_bonus_options?.from.options)
+  if(answers.length < source.choose) {
+    console.log(source.from.options)
     if (e.target.checked) {
       console.log(e.target.value)
-      setAnswers((prev) => [...prev, ...raceInfo?.ability_bonus_options?.from?.options.filter((option) => option.ability_score.name == e.target.value)])
+      setAnswers((prev) => [...prev, ...source.from?.options.filter((option) => {
+        if (option.ability_score) {
+          return option.ability_score.name == e.target.value
+        }
+        if (option.item) {
+          const op = option.item.name.split(':')[1]
+          return op == e.target.value
+        }
+
+      })])
     }
     else {
       setAnswers((prev) => {
@@ -51,7 +68,16 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
     if (!e.target.checked) {
       setAnswers((prev) => {
         const updatedAnswer = [...prev]
-        const index = updatedAnswer.findIndex((value) => value == e.target.value)
+        const index = updatedAnswer.findIndex((value) => {
+          if (value.ability_score) {
+            return value.ability_score.name == e.target.value
+          }
+          if (value.item) {
+            const op = value.item.name.split(':')[1]
+            return op == e.target.value
+          }
+          
+        })
         console.log(index)
         updatedAnswer.splice(index, 1)
         return updatedAnswer
@@ -62,12 +88,6 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
   
 
 }
-  //this handler must limit the number of checked options
-
-  // name bonus, number of options and max number of choices
-
-  
-
 
   
 // API call
@@ -83,7 +103,13 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
         setSubRaceList(res?.data?.subraces)
       }
       if (formState == 3) {
-        setSubRaceInfo(res.data)
+        setSubRaceInfo(res?.data)
+      }
+      if (formState == 5) {
+        setClassesList(res?.data?.results)
+      }
+      if (formState == 6) {
+        setClassInfo(res?.data)
       }
     }
     getFromAPI()
@@ -110,14 +136,39 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
     }
     if (formState == 3) {
       setAbilityBonus(raceInfo.ability_bonuses)
+      setAnswers([])
     }
-
     if (formState == 4) {
+      setAnswers([])
       if (answers.length > 0) {
         setAbilityBonus((prev) => {
           return [...prev, ...answers]
         })
       }
+    }
+    if (formState == 5) {
+      setUrl('https://www.dnd5eapi.co/api/classes')
+    }
+    if (formState == 7) {
+      setProficiencies(() => {
+        const proficienciesFilter = classInfo?.proficiencies.filter((proficiency) => !proficiency.name.startsWith('Saving Throw'))
+        return proficienciesFilter.map((proficiency) => proficiency.name)
+      })
+      setAnswers([])
+    }
+    if (formState == 8) {
+      setProficiencies((prev) => {
+        return [...prev, ...answers.map((pro) => pro.item.name.split(':')[1])]
+      })
+
+      setAtributes({
+        STR: 8,
+        CON: 8,
+        DEX: 8,
+        INT: 8,
+        WIS: 8,
+        CHA: 8
+      })
     }
     
   }, [formState])
@@ -158,6 +209,25 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
     })
   }, [subRaceInfo])
 
+
+  // Class info
+  useEffect(() => {
+    console.log(classInfo)
+
+    if (formState == 6) {
+      setHitDie(classInfo?.hit_die)
+
+      
+
+      setEquipment(classInfo?.starting_equipment)
+
+      setAnswers([])
+
+      setFormState((prev) => prev+1)
+    }
+    
+  }, [classInfo])
+
   useEffect(() => {
     console.log(abilityBonus)
   }, [abilityBonus])
@@ -173,6 +243,13 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
   useEffect(() => {
     console.log(answers)
   }, [answers])
+  useEffect(() => {
+    console.log(classesList)
+  }, [classesList])
+  useEffect(() => {
+    console.log(proficiencies)
+  }, [proficiencies])
+
     
 
 
@@ -191,6 +268,25 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
     setUrl(`https://www.dnd5eapi.co${subRace?.url}`)
   }
 
+  const chooseClass = (classes) => {
+    setClasses(classes?.name)
+
+    setFormState((prev) => prev+1)
+    setUrl(`https://www.dnd5eapi.co${classes?.url}`)
+  }
+
+  const atributesTable = () => {
+    return (
+      <>
+      <input type="number" min={8} max={20} name="STR" id="" step={1} value={atributes.STR} onChange={(e) => setAtributes({...atributes, STR: e.target.value})}/>
+      <input type="number" min={8} max={20} name="CON" id="" step={1} value={atributes.CON} onChange={(e) => setAtributes({...atributes, CON: e.target.value})}/>
+      <input type="number" min={8} max={20} name="DEX" id="" step={1} value={atributes.DEX} onChange={(e) => setAtributes({...atributes, DEX: e.target.value})}/>
+      <input type="number" min={8} max={20} name="INT" id="" step={1} value={atributes.INT} onChange={(e) => setAtributes({...atributes, INT: e.target.value})}/>
+      <input type="number" min={8} max={20} name="WIS" id="" step={1} value={atributes.WIS} onChange={(e) => setAtributes({...atributes, WIS: e.target.value})}/>
+      <input type="number" min={8} max={20} name="CHA" id="" step={1} value={atributes.CHA} onChange={(e) => setAtributes({...atributes, CHA: e.target.value})}/>
+      </>
+    )
+  }
   
 
   return(
@@ -204,17 +300,28 @@ const changeAbilityBonus=(e) =>{//{name,bonus}
         />
         {abilityBonus?.map((ability, id) => <input key={id} type="text" value={`${ability.ability_score.name} ${ability.bonus}`}/>)}
         {lenguages ? <input type="text" value={lenguages}/> : null}
-        {raceInfo?.ability_bonus_options ? <CheckboxForm name='ability_score' options={raceInfo?.ability_bonus_options} onChange={(e) => changeAbilityBonus(e)}/> : null}
+        {raceInfo?.ability_bonus_options ? <CheckboxForm name='ability_score' options={raceInfo?.ability_bonus_options} onChange={(e) => changeAbilityBonus(e, raceInfo?.ability_bonus_options)}/> : null}
         {traits ? <input type="text" value={traits}/> : null}
+        <input type="text"
+        value={classes}/>
+
+        <input type="number" 
+        value={hitDie}/>
+
+        {formState == 7 ? <CheckboxForm name='proficency' options={classInfo?.proficiency_choices[0]} onChange={(e) => changeAbilityBonus(e, classInfo?.proficiency_choices[0])} /> : null}
+        {proficiencies ? proficiencies.map((proficiency) => <input type="text" value={proficiency} /> ): null}
+        {equipment ? equipment.map((equipment) => <input type="text" value={equipment.equipment.name}/> ) : null}
+        {formState == 8 ? atributesTable() : null}
       </form>
 
       
       {formState == 1 ? raceList?.map((race) => <ButtonForm key={race.name} text={race.name} onClick={() => chooseRace(race)}/>) : null}
       {formState == 2 ? subRaceList?.map((subRaces) => <ButtonForm key={subRaces.name} text={subRaces?.name} onClick={() => chooseSubRace(subRaces)}></ButtonForm>) : null}
+      {formState == 5 ? classesList?.map((classes) => <ButtonForm key={classes.name} text={classes?.name} onClick={() => chooseClass(classes)}></ButtonForm>) : null}
       
 
       {formState > 1 ? <button type="button" onClick={() => setFormState((prev) => prev-1)}>Back</button> : null}
-      {formState > 1 ? <button type="button" onClick={() => setFormState((prev) => prev+1)}>Next</button> : null}
+      {formState > 1 && formState < 9 ? <button type="button" onClick={() => setFormState((prev) => prev+1)}>Next</button> : null}
     </>
   )
 }
