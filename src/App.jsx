93b123698eef2748ffaad2a3174a17c045c5560b
mattos-rafael <rafa.rafa.rafa07@gmail.com>
2026-05-11 16:09:43 +0200

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import LayoutDefault from './template/LayoutDefault/LayoutDefault'
 import CreateCharacterForm from './components/CreateCharacterForm/CreateCharacterForm'
+import Login from './components/Login/Login'
 
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
     <LayoutDefault>
       <main>
         <Routes>
-          <Route path='/' element={<CreateCharacterForm />} />
-   
+          <Route path='/create-character' element={<CreateCharacterForm />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </main>
     </LayoutDefault>
